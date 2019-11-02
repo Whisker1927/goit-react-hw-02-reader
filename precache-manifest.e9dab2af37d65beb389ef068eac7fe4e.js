eb0094e5b50f2f69dec5f2a88790878a54b66029
Vladyslav Bezkrylyi <whisker1927@gmail.com>
2019-11-02 18:38:14 +0200

    Updates

---
+++ precache-manifest.e9dab2af37d65beb389ef068eac7fe4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7748c0e3f37e361cc2e6ac2a2a4354",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "5a7d613c5715c1890c23",
+    "url": "/goit-react-hw-02-reader/static/css/main.72744eec.chunk.css"
+  },
+  {
+    "revision": "6ef2c4717bfdda3784f0",
+    "url": "/goit-react-hw-02-reader/static/js/2.254b4c81.chunk.js"
+  },
+  {
+    "revision": "5a7d613c5715c1890c23",
+    "url": "/goit-react-hw-02-reader/static/js/main.23ef996f.chunk.js"
+  },
+  {
+    "revision": "0da2320c246f0dcbed6e",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.21ebc90b.js"
+  }
+]);
